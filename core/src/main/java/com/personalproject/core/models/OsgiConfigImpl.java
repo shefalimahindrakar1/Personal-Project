4ccedd8db8ff5aaefc24b6873853019164c357d5
Shefali Mahindrakar <shefali.mahindrakar@ugamsolutions.com>
2021-12-03 15:24:55 +0530

    osgi and runmodes

---
+++ core/src/main/java/com/personalproject/core/models/OsgiConfigImpl.java
@@ -0,0 +1,88 @@
+package com.personalproject.core.models;
+
+import org.osgi.service.component.annotations.Activate;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.metatype.annotations.*;
+
+@Component(service = OsgiConfig.class,immediate = true)
+@Designate(ocd = OsgiConfigImpl.ServiceConfig.class )
+public class OsgiConfigImpl implements OsgiConfig {
+
+    @ObjectClassDefinition(name="Personal Project - Osgi Configuration",
+            description = "Osgi Configuration demo.")
+    public @interface ServiceConfig {
+
+        @AttributeDefinition(
+                name = "Service Name",
+                description = "Enter service name.",
+                type = AttributeType.STRING)
+        public String serviceName() default "Personal Project Service";
+
+        @AttributeDefinition(
+                name = "Service Count",
+                description = "Add Service Count.",
+                type = AttributeType.INTEGER
+        )
+        int getServiceCount() default 5;
+
+        @AttributeDefinition(
+                name = "Live Data",
+                description = "Check this to get live data.",
+                type = AttributeType.BOOLEAN)
+        boolean getLiveData() default false;
+
+        @AttributeDefinition(
+                name = "Countries",
+                description = "Add countries locales for which you want to run this service.",
+                type = AttributeType.STRING
+        )
+        String[] getCountries() default {"de","pu"};
+
+        @AttributeDefinition(
+                name = "Run Modes",
+                description = "Select Run Mode.",
+                options = {
+                        @Option(label = "Author",value = "author"),
+                        @Option(label = "Publish",value = "publish"),
+                        @Option(label = "Both",value = "both")
+                },
+                type = AttributeType.STRING)
+        String getRunMode() default "both";
+    }
+
+    private String serviceName;
+    private int serviceCount;
+    private boolean liveData;
+    private String[] countries;
+    private String runModes;
+
+    @Activate
+    protected void activate(ServiceConfig serviceConfig){
+        serviceName=serviceConfig.serviceName();
+        serviceCount=serviceConfig.getServiceCount();
+        liveData=serviceConfig.getLiveData();
+        countries=serviceConfig.getCountries();
+        runModes=serviceConfig.getRunMode();
+    }
+
+    @Override
+    public String getServiceName() {
+        return serviceName;
+    }
+    @Override
+    public int getServiceCount() {
+        return serviceCount;
+    }
+    @Override
+    public boolean isLiveData() {
+        return liveData;
+    }
+    @Override
+    public String[] getCountries() {
+        return countries;
+    }
+    @Override
+    public String getRunModes() {
+        return runModes;
+    }
+}
